864fecee8b965efc2411b8b02d53455099a840a3
camargo-rubcube <guilherme.camargo@rubcube.com>
2023-08-30 00:56:42 -0300

    feat: create and insert table

--- Trabalho.c
+++ Trabalho.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <conio2.h>
 #include "TADTrabalho.h"
+
+void InsereTabelaNoBancoDeDados (bd * * bancoDeDados, tabela * * tabela ) {
+	tabela -> ant = NULL;
+	tabela -> prox = NULL;
+	if((* bancoDeDados) -> listaTabela  == NULL) {
+		(* bancoDeDados) -> listaTabela = tabela;
+	}
+	else {
+		tabela * aux;
+		aux = (* bancoDeDados) -> listaTabela;
+		while( aux -> prox != NULL) {
+			aux = aux -> prox;
+		}
+		aux -> prox = * tabela;
+		(* tabela) -> ant = aux;
+	}
+}
+
+
+void CriarTabela (bd * * bancoDeDados, char nomeTabela[50], atributo * atributos ) {
+	tabela * novaTabela = (tabela*)malloc(sizeof(tabela));
+	strcpy((novaTabela -> nometabela), nomeTabela);
+	novaTabela -> listaAtributos = atributos;
+	InsereTabelaNoBancoDeDados(bancoDeDados, &novaTabela);
+}
+
+
+
 
 int main()
 {
